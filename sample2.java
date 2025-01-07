28a6242bba34a42d565e600c689dbca2c7456de4
Aisha-Sharma <167778121+Aisha-Sharma@users.noreply.github.com>
2025-01-07 12:29:37 +0530

    Create sample2.java

---
+++ sample2.java
@@ -0,0 +1,21 @@
+public class Ex5 {
+    public static void main(String[] args) {
+        Integer num1 = 1000;
+        Integer num2 = 1000;
+
+        if (num1 == num2) {
+            System.out.println("num1 and num2 are equal using '==' operator.");
+        } else {
+            System.out.println("num1 and num2 are not equal using '==' operator.");
+        }
+
+        String text = "hello";
+        String anotherText = new String("hello");
+
+        if (text == anotherText) {
+            System.out.println("text and anotherText are the same using '==' operator.");
+        } else {
+            System.out.println("text and anotherText are not the same using '==' operator.");
+        }
+    }
+}
